f925773f2f2ac320b95d1c8fbb6ab5c6cee77b79
Maxime Lefebvre <mlefebvre@taxelco.com>
2018-10-25 01:26:31 -0400

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const port = process.env.PORT || 3000;
+
 io.on('connection', function(socket){
   console.log('CONNECTED');
   socket.on('chat message', function(msg){
@@ -18,6 +20,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/quizzer-app/dist/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log(`listening on *: ${port}`);
 });
